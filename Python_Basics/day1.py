3cc9025d03ae21c17fc175d307321de5f0bf4061
Vivek E <vivekeleyidath@gmail.com>
2026-07-04 00:02:14 +0530

    Fix README filename

---
+++ Python_Basics/day1.py
@@ -0,0 +1,29 @@
+#=======================================================================================
+#                           my first day first python program
+#=======================================================================================
+
+print("hello world")
+#output: hello world
+
+
+print(type("hello world"))
+#output: <class 'str'>
+
+
+print(type(10))
+# output: <class 'int'>
+
+
+print(type(10.5))
+# output: <class 'float'>
+
+
+print(type(True))
+output: <class 'bool'>
+
+
+#==========================================================================================
+
+
+
+
