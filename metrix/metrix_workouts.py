434531ccfff18e32a4528f669f66edd13a945d79
Vivek E <vivekeleyidath@gmail.com>
2026-07-16 18:12:03 +0530

    Add matrix multiplication program with user input

--- metrix/metrix_workouts.py
+++ metrix/metrix_workouts.py
@@ -13,7 +13,7 @@
 #        7    8    9  
 #========================================================================================================================================
 
-#PROBLOM 1 :   READ TWO METRIX AND PRINT ITS RESULT METRIX.
+#PROBLOM 1 :                              READ TWO METRIX AND PRINT ITS RESULT METRIX.
 
 #=========================================================================================================================================
 # #solution:
@@ -68,7 +68,7 @@
 #     print("\n")
 #========================================================================================================================================
 
-#PROBLOM 2 :   READ TWO METRIX AND PRINT ITS DEFFERENCE METRIX.(SUBSTACTION)
+#PROBLOM 2 :                    READ TWO METRIX AND PRINT ITS DEFFERENCE METRIX.(SUBSTACTION)
 
 #=========================================================================================================================================
 #solution:
@@ -133,28 +133,84 @@
 #  [M1] 0   1   2   |    X    #  [M2] 3   0   1   | ========>   # [result]  5    4    1    |        
 #       3   0   2   |         #       1   2   0   |             #           8    7    0    |      
 #-------------------#         #-------------------#             #--------------------------#    
-m1=[[1, 2, 1], [0, 1, 2], [3, 0, 2]]
-m2=[[2, 1, 0], [3, 0, 1], [1, 2, 0]]
-result_main_lst=[]
-for i in range(3):
-    sub_lst=[]
+# m1=[[1, 2, 1], [0, 1, 2], [3, 0, 2]]
+# m2=[[2, 1, 0], [3, 0, 1], [1, 2, 0]]
+# result_main_lst=[]
+# for i in range(len(m1)):
+#     sub_lst=[]
 
-    for j in range(3):
-        r=0
-        for k in range(3):
-            r+=m1[i][k]*m2[k][j]
-        sub_lst.append(r)
-    result_main_lst.append(sub_lst)
-for r in range(3):
-    for c in range(3):
-        print(result_main_lst[r][c],end="   ")
-    print()
+#     for j in range(len(m2)):
+#         total=0
+#         for k in range(len(m2)):
+#             total+=m1[i][k]*m2[k][j]
+#         sub_lst.append(total)
+#     result_main_lst.append(sub_lst)
+# for r in range(3):
+#     for c in range(3):
+#         print(result_main_lst[r][c],end="   ")
+#     print()
 
 #output:   9   3   2   
 #          5   4   1   
 #          8   7   0  
 #=========================================================================================================================================
+
+#PROBLOM 3 :                               METRIX MULTIPLICATION  FROM USER INPUT
+
+#=========================================================================================================================================
+#solution: 
+
+m1=[]
+m2=[]
+product=[]
+rows=int(input("enter the number of ROW'S for THE METRIX     :"))
+columns=int(input("enter the number of COLUMN'S for THE METRIX  :"))
+m1_rows=rows
+m1_columns=columns
+for row in range(rows):
+    sub=[]
+    for col in range(columns):
+        ele=int(input("enter data for the first metrix:"))
+        sub.append(ele)
+    m1.append(sub)
+print(m1)
+
+rows=int(input("enter number of columns of the 2nd metrix:"))
+if rows==m1_rows:
+    columns=int(input("enter the number of columns for the 2nd metrix:"))
+    if columns==m1_columns:
+        for row in range(rows):
+            sub=[]
+            for col in range(columns):
+                element=int(input("enter the elements for the 2nd mtrix:"))
+                sub.append(element)
+            m2.append(sub)
+        print(m2)
+    else:
+        print("ENTERD NUMBER OF COLUMNS ARE NOT MATCHING WITH 1ST METRIX...!!")
+
+else:
+    print("number of rows are not matching.....!!!!")
+for i in range(len(m1)):
+    sub=[]
+    for j in range(len(m2)):
+        total=0
+        for k in range(len(m2)):
+            total+=m1[i][k]*m2[k][j]
+        sub.append(total)
+    product.append(sub)
+
+for i in range(len(product)):
+    for j in range(len(product)):
+        print(product[i][j],end="   ")
+    print( )
         
+
+
+
+
+
+
             
 
     
